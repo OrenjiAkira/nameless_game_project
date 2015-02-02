6cb576cbd0de0fdc81610dc783ebeca9433ba2e0
Akira Mizutani <orenjiakira@gmail.com>
2015-02-02 21:49:46 -0200

    Organised Translate Property

--- game/Property/Properties/Translate.lua
+++ game/Property/Properties/Translate.lua
@@ -8,29 +8,39 @@ function Translate(self, element)
 	self:setName('Translate')
 
 	-- local variables
-	local x, y = 0, 0
+	local x, y = 0, 0 --not world position, but render position
 	local playerheight = elements['player']:getProperty('Sprite'):getQuadHeight()
 
+	-- local methods
+	local function constructor( ... )
+		local pos = elements['player']:getProperty('Position'):getPos()
+		element:getProperty('Position'):setPos(pos.x, pos.y)
+	end
+
+	-- public methods
 	function self:setTranslate(ax,ay)
 		element:getProperty('Position'):setPos(ax,ay)
+		-- set min or max to hitbox in the future
 		x = ax*unit
 		y = ay*unit
 	end
+	
 	function self:getTranslate()
 		return { x, y }
 	end
 
+	-- update methods
 	function self:update()
-		local pos = elements['player']:getProperty('Position'):getPos()
+		local pos = element:getProperty('Position'):getPos()
 		self:setTranslate( pos.x, pos.y )
 	end
 
-	-- public methods
 	function self:render()
 		--love.graphics.push()
 		love.graphics.translate( -x +window:getWidth()/2, -y + window:getHeight()/2 + playerheight - 8*zoom)
 		--love.graphics.pop()
 	end
 
+	constructor()
 	return self
 end
