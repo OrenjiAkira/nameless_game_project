b202bf8a03811ff1812910da880f635efeb14a23
Akira Mizutani <orenjiakira@gmail.com>
2015-05-19 13:11:43 -0300

    Deleted camera element files

--- game/main.lua
+++ game/main.lua
@@ -7,7 +7,7 @@ require 'input'
 
 require 'Element/Element'
 require 'Element/ElementList'
-require 'Element/Elements/Camera'
+--require 'Element/Elements/Camera'
 
 require 'Property/Property'
 require 'Property/Properties/Sprite'
